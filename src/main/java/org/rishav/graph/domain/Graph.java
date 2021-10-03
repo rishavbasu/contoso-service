eaeec02c3c339d5da7548e7372c2d5e6b301b85b
basur <basur@LAPTOP-OASC4DJI.mshome.net>
2021-10-04 02:50:31 +0530

    fixed dfs cycle

--- src/main/java/org/rishav/graph/domain/Graph.java
+++ src/main/java/org/rishav/graph/domain/Graph.java
@@ -29,7 +29,6 @@ public class Graph<T, Q extends Number> {
 	public void addEdge(Vertex<T> v1, Vertex<T> v2, Q distance) {
 		// TODO add exception
 		adjacencyLists.get(v1).put(v2, distance);
-		adjacencyLists.get(v2).put(v1, distance);
 	}
 
 	/**
@@ -44,33 +43,36 @@ public class Graph<T, Q extends Number> {
 	}
 
 	// Prints all paths from source to destination
-	public void printAllPaths(Vertex<T> source, Vertex<T> destination) {
+	public void printAllPaths(Vertex<T> source, Vertex<T> destination, int depth) {
 		Map<Vertex<T>, Boolean> visited = new HashMap<Vertex<T>, Boolean>();
 		List<Vertex<T>> currentPath = new ArrayList<>();
 
-		currentPath.add(source);
-		dfs(source, destination, visited, currentPath);
+		 currentPath.add(source);
+		dfs(source, destination, visited, currentPath, depth);
 		System.out.println(currentPath);
 	}
 
 	private void dfs(Vertex<T> source, Vertex<T> destination, Map<Vertex<T>, Boolean> visited,
-			List<Vertex<T>> currentPath) {
+			List<Vertex<T>> currentPath, int depth) {
+
+		int currentDepth = 1;
 		visited.put(source, Boolean.TRUE); // to avoid cycle
 
 		if (source.equals(destination)) {
 			System.out.println(currentPath);
-			return;
-		}
+		} else if(currentDepth < depth){
 
-		Map<Vertex<T>, Q> adjacentVertices = getAdjacentVertices(source);
-		for (Vertex<T> vertex : adjacentVertices.keySet()) {
-			if (visited.get(vertex) == null || !visited.get(vertex)) {
-				currentPath.add(vertex);
-				dfs(vertex, destination, visited, currentPath);
-				currentPath.remove(vertex);
+			Map<Vertex<T>, Q> adjacentVertices = getAdjacentVertices(source);
+			for (Vertex<T> vertex : adjacentVertices.keySet()) {
+				if (visited.get(vertex) == null || !visited.get(vertex)) {
+					currentPath.add(vertex);
+					dfs(vertex, destination, visited, currentPath, currentDepth++);
+					currentPath.remove(vertex);
+				}
 			}
+			currentPath.remove(source);
 		}
-
+		
 		visited.put(source, Boolean.FALSE);
 	}
 
